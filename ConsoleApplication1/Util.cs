0515fa63f318d9ce716706bcc3b98d96aebdd7d4
suiheilibe <ntjapanize@yahoo.co.jp>
2015-05-05 01:35:45 +0900

    RulesToDictionary

--- ConsoleApplication1/Util.cs
+++ ConsoleApplication1/Util.cs
@@ -7,9 +7,12 @@ using System.IO;
 
 namespace AppRunningTimer
 {
+    using RulesDictionary = Dictionary<string, Rule>;
+
     class Util
     {
         /// <summary>
+        /// Get normalized path.
         /// http://stackoverflow.com/questions/1266674/how-can-one-get-an-absolute-or-normalized-file-path-in-net
         /// </summary>
         /// <param name="path"></param>
@@ -21,12 +24,35 @@ namespace AppRunningTimer
                        .ToUpperInvariant();
         }
 
-        public static Dictionary<string,Rule> RulesToDictionary(Rule[] rules)
+        /// <summary>
+        /// Converts `Rule` array to a new dictionary.
+        /// </summary>
+        /// <param name="rules"></param>
+        /// <returns></returns>
+        public static RulesDictionary RulesToDictionary(Rule[] rules)
         {
+            var dict = new Dictionary<string, Rule>();
             foreach (var rule in rules) {
-                
+                var normalizedPath = rule.normalizedPath;
+                if (dict.ContainsKey(normalizedPath))
+                {
+                    // TODO: 重複ルールの扱い
+                }
+                else
+                {
+                    dict.Add(normalizedPath, rule);
+                }
             }
-            return null;
+            return dict;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dict"></param>
+        public static void MatchProcessesWithRules(RulesDictionary dict)
+        {
+
         }
     }
 }
