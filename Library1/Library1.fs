4f1a98b2419d46db5d8c08d35662e8804229b012
suiheilibe <ntjapanize@yahoo.co.jp>
2015-07-22 23:14:50 +0900

    Visual Studio 2015

---
+++ Library1/Library1.fs
@@ -0,0 +1,13 @@
+﻿namespace AppRunningTimer
+
+module PathUtil = 
+    open System
+    open System.IO
+    
+    type NormalizedPath = 
+        { RealPath : string }
+    
+    let makeNormalizedPath path = 
+        { RealPath = 
+              Path.GetFullPath((new Uri(path)).LocalPath)
+                  .TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar).ToUpperInvariant() }
