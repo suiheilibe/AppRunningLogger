b10b9f14cd2e400dae78a5d3d41129298120a0ba
suiheilibe <ntjapanize@yahoo.co.jp>
2015-08-01 00:08:00 +0900

    Make `NormalizedPath` to be a class

--- Library1/Library1.fs
+++ Library1/Library1.fs
@@ -1,21 +1,20 @@
 ﻿namespace AppRunningTimer
 
-type NormalizedPath = 
-    { RealPath : string }
+open System
+open System.IO
+
+type NormalizedPath(path) =
+    let normalizedPath = Path.GetFullPath((new Uri(path)).LocalPath)
+                          .TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar).ToUpperInvariant()
+    member this.RealPath with get () = normalizedPath
 
 type Rule = 
     { AppId : uint64
       NormalizedPath : NormalizedPath
       Enabled : bool }
 
-module PathUtil = 
-    open System
-    open System.IO
+type RuleX(appId, normalizedPath, enabled) = class end
     
-    let makeNormalizedPath path = 
-        { RealPath = 
-              Path.GetFullPath((new Uri(path)).LocalPath)
-                  .TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar).ToUpperInvariant() }
 
 module Main =
-    let rules = [|{AppId = 0UL; NormalizedPath = PathUtil.makeNormalizedPath "F:\\GAMES\\mu100\\murasaki.exe"; Enabled = true}|];
+    let rules = [|{AppId = 0UL; NormalizedPath = NormalizedPath "F:\\GAMES\\mu100\\murasaki.exe"; Enabled = true}|];
