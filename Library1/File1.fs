f6fd283aacdccebfc21ff9c8bb436d5daae49cf0
suiheilibe <ntjapanize@yahoo.co.jp>
2015-12-15 21:32:57 +0900

    mainLoop

--- Library1/File1.fs
+++ Library1/File1.fs
@@ -1,3 +1,16 @@
 ﻿module AppRunningTimer.File1
 
+open System.Diagnostics
+open System.Threading
+
 let rules = [|{AppId = 0UL; NormalizedPath = NormalizedPath "F:\\GAMES\\mu100\\murasaki.exe"; Enabled = true}|];
+
+let mainLoop =
+    while true do
+        let processes = Process.GetProcesses()
+        for p in processes do
+            try
+                let npath = NormalizedPath p.MainModule.FileName
+                ()
+            with | e -> ()
+            Thread.Sleep 1000
