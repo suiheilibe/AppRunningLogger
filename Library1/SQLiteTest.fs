eb01fd73a84d16ca67c8f10638ccf8f7925d01b3
suiheilibe <ntjapanize@yahoo.co.jp>
2015-12-21 21:40:40 +0900

    PRAGMA test

--- Library1/SQLiteTest.fs
+++ Library1/SQLiteTest.fs
@@ -8,17 +8,17 @@ open System
 open System.Threading.Tasks
 
 type App () =
-    [<PrimaryKey>]
-    member val Id : uint64 = 0UL with get, set
+    [<PrimaryKey;AutoIncrement>]
+    member val Id : int64 = 0L with get, set
     [<Indexed>]
     member val CanonicalPath : string = "" with get, set
 
 type AppRunningLog () =
     static let dtUnder = DateTime.MinValue
     [<PrimaryKey;AutoIncrement>]
-    member val Id : uint64 = 0UL with get, set
+    member val Id : int64 = 0L with get, set
     [<NotNull;Indexed>]
-    member val AppId : uint64 = 0UL with get, set
+    member val AppId : int64 = 0L with get, set
     member val Active : bool = false with get, set
     member val Begin : DateTime = dtUnder with get, set
     member val End : DateTime = dtUnder with get, set
@@ -30,12 +30,15 @@ let conn =
         true
     )
 
+conn.ExecuteScalar<string>("PRAGMA synchronous = NORMAL") |> ignore
+conn.ExecuteScalar<string>("PRAGMA journal_mode = WAL") |> ignore
+
 let d = conn.CreateTable<App>()
 
 printfn "result: %d" d
 
 [1 .. 5]
-    |> Seq.iter (fun _ ->
+    |> Seq.iter (fun i ->
         conn.Insert(new App(CanonicalPath = "This is a test string")) |> ignore
     )
 
