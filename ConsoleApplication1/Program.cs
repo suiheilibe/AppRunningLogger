64b5d54d23095dddcaff1471f1c843a84a745a8f
suiheilibe <ntjapanize@yahoo.co.jp>
2015-12-27 13:03:49 +0900

    Cleanup

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -12,48 +12,6 @@ namespace AppRunningLogger
     {
         static void Main(string[] args)
         {
-            Rule[] rules = null;
-            try
-            {
-                rules = new[] { new Rule(0, new NormalizedPath("F:\\GAMES\\mu100\\murasaki.exe"), true) };
-            }
-            catch (Exception e)
-            {
-                // TODO: 例外記録メソッド
-                Console.WriteLine("An error occurred: '{0}'", e);
-            }
-            if (rules == null) {
-                rules = new Rule[0];
-            }
-            var rulesDict = Util.RulesToDictionary(rules);
-            var cnt = 0;
-            while (true) {
-                var processes = GetProcesses();
-                foreach (var p in processes)
-                {
-                    try
-                    {
-                        var npath = new NormalizedPath(p.MainModule.FileName);
-                        if (p.MainWindowTitle == "murasaki")
-                        {
-                            cnt++;
-                            Console.WriteLine(cnt);
-                            var module = p.MainModule;
-
-                            Console.WriteLine(module.FileName);
-                        }
-                    }
-                    catch (Exception e)
-                    {
-
-                    }
-                }
-                Thread.Sleep(1000);
-            }
-        }
-        static Process[] GetProcesses()
-        {
-            return Process.GetProcesses();
         }
     }
 }
