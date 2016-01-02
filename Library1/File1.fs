1c5214db0206e576315bbc5ad908aca722adf900
suiheilibe <ntjapanize@yahoo.co.jp>
2016-01-02 23:06:32 +0900

    Use `list`

--- Library1/File1.fs
+++ Library1/File1.fs
@@ -6,7 +6,7 @@ open System.Collections.Generic
 
 type AppRunningLoggerState =
     { Connection     : SQLiteTest.DBConnection
-      AppDefinitions : SQLiteTest.AppDefinition []
+      AppDefinitions : SQLiteTest.AppDefinition list
     }
 
 let getProcesses = Process.GetProcesses : unit -> Process []
@@ -17,39 +17,39 @@ let rec mainLoop (state : AppRunningLoggerState) =
     let appDefs = state.AppDefinitions
     let appDict =
         appDefs
-        |> Array.map (fun x ->
+        |> List.map (fun x ->
             try
                 Some (canonicalize x.Path, x)
             with | ex -> None
             )
-        |> Array.choose id
+        |> List.choose id
         |> dict
-    let procs = getProcesses ()
+    let procs = getProcesses () |> List.ofArray
     let procPathPairs =
         procs
-        |> Array.map (fun x ->
+        |> List.map (fun x ->
             try
                 x.MainWindowHandle |> ignore
                 let fileName = x.MainModule.FileName
                 Some (canonicalize fileName, fileName) // (<Canonicalized path>, <`Process` path>)
             with | ex -> None
             )
-        |> Array.choose id
+        |> List.choose id
     let newAppDefs =
         procPathPairs
-        |> Array.filter (fun x -> fst x |> appDict.ContainsKey |> not)
-        |> Array.map (fun x -> SQLiteTest.AppDefinition(Path = snd x))
+        |> List.filter (fun x -> fst x |> appDict.ContainsKey |> not)
+        |> List.map (fun x -> SQLiteTest.AppDefinition(Path = snd x))
     newAppDefs
     |> SQLiteTest.addAppDefinition state.Connection
     printfn "new: %d" newAppDefs.Length
-    let nextAppDefs = Array.append appDefs newAppDefs
+    let nextAppDefs = List.append newAppDefs appDefs
     Thread.Sleep 1000
     mainLoop { Connection = state.Connection; AppDefinitions = nextAppDefs }
    
 let startMainLoop () =
     try
         let conn = SQLiteTest.initMainDB ()
-        let appDefs = SQLiteTest.getAppDefinition conn |> Array.ofSeq
+        let appDefs = SQLiteTest.getAppDefinition conn |> List.ofSeq
         mainLoop { Connection = conn; AppDefinitions = appDefs }
     with | ex -> printfn "%s" ex.StackTrace
     ()
