21ce9962c04777d560a507b93874efd5c05e8d56
suiheilibe <ntjapanize@yahoo.co.jp>
2016-01-10 19:59:15 +0900

    Cosmetics

--- Library1/Library1.fs
+++ Library1/Library1.fs
@@ -6,15 +6,14 @@ open System.IO
 /// <summary>
 /// 
 /// </summary>
-type CanonicalPath(path) =
-    let canonicalPath =
-        Path.GetFullPath((new Uri(path)).LocalPath)
-            .TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar)
+type CanonicalPath(path) = 
+    let canonicalPath = 
+        Path.GetFullPath((new Uri(path)).LocalPath).TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar)
             .ToUpperInvariant()
-    member this.RawPath with get () = canonicalPath
+    member this.RawPath = canonicalPath
     override this.GetHashCode() = this.RawPath.GetHashCode()
-    override this.Equals(obj : obj) =
-        match obj with
+    override this.Equals(o : obj) = 
+        match o with
         | null -> false
         | :? CanonicalPath as cpath -> this.RawPath.Equals(cpath.RawPath)
         | _ -> false
