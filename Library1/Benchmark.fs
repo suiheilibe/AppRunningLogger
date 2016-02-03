38c7e9d67d63318660e8e738eeb2215c51b1a37f
suiheilibe <ntjapanize@yahoo.co.jp>
2016-02-04 00:23:39 +0900

    `TestTable`

--- Library1/Benchmark.fs
+++ Library1/Benchmark.fs
@@ -1,5 +1,13 @@
 ﻿module AppRunningLogger.Benchmark
 
+open SQLite.Net.Attributes
+
+type TestTable () =
+    [<PrimaryKey;Unique>]
+    member val Id : int64 = 0L with get, set
+    [<Unique>]
+    member val Path : string = "" with get, set
+
 let test () =
     let conn = SQLiteTest.newSQLiteConnection "benchmark.db"
     ()
