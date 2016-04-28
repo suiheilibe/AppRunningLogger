49df08b46384e9f1087b698027d3a64e60b44e78
suiheilibe <ntjapanize@yahoo.co.jp>
2016-04-29 00:23:21 +0900

    CanonicalPath

--- ConsoleApplication1/CanonicalPath.cs
+++ ConsoleApplication1/CanonicalPath.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,11 @@ namespace AppRunningLogger
 {
     class CanonicalPath
     {
+        string canonicalPath;
+        CanonicalPath(string path)
+        {
+            canonicalPath = Path.GetFullPath((new Uri(path)).LocalPath).TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar)
+            .ToUpperInvariant();
+        }
     }
 }
