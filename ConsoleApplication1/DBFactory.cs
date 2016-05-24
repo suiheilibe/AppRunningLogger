03b869e43e5bf07d147c279b8fe52205aa25b66b
suiheilibe <ntjapanize@yahoo.co.jp>
2016-05-24 23:58:37 +0900

    using

--- ConsoleApplication1/DBFactory.cs
+++ ConsoleApplication1/DBFactory.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite.Net;
+//using SQLite.Net.Attributes;
+//using SQLite.Net.Interop;
+using SQLite.Net.Platform.Win32;
 
 namespace AppRunningLogger
 {
