13c9ffa4deb18ccd554a34d12f4bebfe5ee7825c
suiheilibe <ntjapanize@yahoo.co.jp>
2016-09-11 13:03:03 +0900

    C# variant of "Win32Api"

---
+++ ConsoleApplication1/Win32Api.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace AppRunningLogger
+{
+    class Win32Api
+    {
+        [DllImport("user32.dll")]
+        extern static IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        extern static UInt32 GetWindowThreadProcessId(IntPtr hWnd, ref UInt32 lpdwProcessId);
+    }
+}
