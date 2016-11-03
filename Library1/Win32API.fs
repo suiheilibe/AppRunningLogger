6493f26e64c010c73cf3d8003554be0589e19ae0
suiheilibe <ntjapanize@yahoo.co.jp>
2016-11-03 19:51:50 +0900

    Remove FS code

--- Library1/Win32API.fs
+++
@@ -1,9 +0,0 @@
-﻿module AppRunningLogger.Win32API
-
-open System.Runtime.InteropServices
-
-[<DllImport("user32.dll")>]
-extern nativeint GetForegroundWindow();
-
-[<DllImport("user32.dll", SetLastError=true)>]
-extern uint32 GetWindowThreadProcessId(nativeint hWnd, uint32& lpdwProcessId);
