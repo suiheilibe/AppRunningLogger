b4fe06ae39bdf100f91ccc11120da1eb30b4fc86
suiheilibe <ntjapanize@yahoo.co.jp>
2016-11-23 19:17:24 +0900

    SQLiteTest

---
+++ ConsoleApplication1/SQLiteTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net;
+using SQLite.Net.Attributes;
+using SQLite.Net.Interop;
+using SQLite.Net.Platform.Win32;
+
+namespace AppRunningLogger
+{
+    class SQLiteTest
+    {
+        class AppDefinition : IComparable<AppDefinition>
+        {
+            [PrimaryKey, Unique]
+            long Id { get; set; } = 0;
+            [Unique]
+            string Path { get; set; } = "";
+
+            public int CompareTo(AppDefinition other)
+            {
+                return Id.CompareTo(other.Id);
+            }
+        }
+
+        class AppRunningLog
+        {
+            [PrimaryKey, AutoIncrement]
+            long Id { get; set; } = 0;
+            [NotNull, Indexed]
+            long AppId { get; set; } = 0;
+            bool Active { get; set; } = false;
+            DateTime Begin { get; set; } = DateTime.MinValue;
+            DateTime End { get; set; } = DateTime.MinValue;
+        }
+    }
+}
